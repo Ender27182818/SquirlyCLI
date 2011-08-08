f4d970e600e0fc122fef6efb8d4711a9040949e9
Eli Ribble <eribble@wavelink.com>
2011-08-08 16:20:24 -0600

    Made the code more c++-esk, now list out all events received. Not sure what they mean though

--- test.cpp
+++ test.cpp
@@ -27,44 +27,54 @@ void perror_exit(char* error)
 
 int main(int argc, char* argv[])
 {
-	struct input_event ev[64];
-	int fd;
-	int rd;
-	int value;
-	int size = sizeof(struct input_event);
-	char name[256] = "Unknown";
-	char* device = NULL;
-
 	// Setup check
 	if( argv[1] == NULL ) {
 		printf( "Please specify the path to the device" );
 		exit(0);
 	}
 
-	if( getuid() != 0 )
-		printf("You are not root! This may not work..");
-
+	char* device = NULL;
 	if( argc > 1 )
 		device = argv[1];
 
-	// Open Device
-	if( ( fd = open(device, O_RDONLY) ) == -1 )
+	// See if the device exists at all
+	struct stat file_info;
+	int return_code = ::stat( device, &file_info );
+	if( return_code != 0 ) {
 		printf( "%s is not a valid device.\n", device );
+		exit(1);
+	}
+
+	// Open Device
+	int file_descriptor = 0;
+	if( ( file_descriptor = open(device, O_RDONLY) ) == -1 )
+	{
+		printf( "%s cannot be opened.\n", device );
+		if( getuid() != 0 )
+			printf( " Try root permissions.\n" );
+		exit(2);
+	}
 
 	// Print Device Name
-	ioctl( fd, EVIOCGNAME (sizeof (name)), name );
+	char name[256] = "Unknown";
+	ioctl( file_descriptor, EVIOCGNAME (sizeof (name)), name );
 	printf( "Reading from %s (%s)\n", device, name );
-
+	
+	// Read events
+	struct input_event events[64];	
 	while(1) {
-		if( (rd = read(fd, ev, size * 64) ) < size )
-			perror_exit( "read()" );
+		int bytes_read = read(file_descriptor, events, sizeof(input_event) * 64);
+		int events_read = bytes_read / sizeof(input_event);
 
-		value = ev[0].value;
-
-		if( value != ' ' && ev[1].value == 1 && ev[1].type == 1 ) {
-			printf( "Code[%d]\n", ev[1].code);
-
+		if( events_read <= 0 ) {
+			printf( "Failed to read any events\n" );
+			exit( 0 );
 		}
+		for( int i = 0; i < events_read; ++i ) {
+			input_event* current_event = events + i;
+			printf( "Event: %d %d %d\n", current_event->type, current_event->code, current_event->value );
+		}
+		printf("---------\n");
 	}
 
 	return 0;
