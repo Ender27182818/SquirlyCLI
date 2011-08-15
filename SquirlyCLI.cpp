b5f084024d1417eb59486ea3ceea003a0fdd4395
Eli Ribble <junk@theribbles.org>
2011-08-15 14:42:34 -0600

    Fixed up the transaction output so that it shows the date, made the output files work in etc, added a daemonizing function - basically it all works as I'd hoped at this point

--- SquirlyCLI.cpp
+++ SquirlyCLI.cpp
@@ -18,9 +18,14 @@
 #include <iostream>
 #include <fstream>
 
+std::ofstream log;
+
+const char* TRANSACTION_FILE = "/etc/Squirly/squirly.transactions";
+const char* LOG_FILE = "/etc/Squirly/squirly.log";
+
 void handler(int sig)
 {
-	printf("\nexiting...(%d)\n", sig);
+	//printf("\nexiting...(%d)\n", sig);
 	exit(0);
 }
 
@@ -30,7 +35,12 @@ void perror_exit(char* error)
 	handler(9);
 }
 
+/// @brief Used to map between key codes and ascii characters. The character at each position is the ascii equivalent of the key code
 const char ASCII_CHARACTERS[] = "  12345678" "90-= 	QWER" "TYUIOP[]\n " "ASDFGHJKL;" "'` \\ZXCVBN" "M,./ *    " "          " " 789-456+1" "23        " "        // "; 
+
+/// @brief Get the ascii character for the given key code
+/// @param[in] code The code to convert
+/// @returns The ascii equivalent of the key code (either capital letter, number or some sybols. Unrecognized or unprintable characters return the null character or a space, respectively
 char getASCII( int code )
 {
 	if( code > 0 && code < (sizeof(ASCII_CHARACTERS)/sizeof(ASCII_CHARACTERS[0])) )
@@ -43,20 +53,52 @@ char getASCII( int code )
 /// @brief Does all the work for a transaction to be fully recorded
 void commitTransaction( const std::string& upc )
 {
+	
+	// Get the current date and time
+	time_t cur_time = ::time( 0 );
+	tm* broken_time = ::localtime( &cur_time );
+	char time_buffer[256];
+	strftime( time_buffer, 256, "%F %H:%M:%S ", broken_time );
+
 	// Open the transaction file
-	std::ofstream f( "/home/eli/squirly.transactions", std::ios_base::app );
-	f << upc << std::endl;
+	std::ofstream f( TRANSACTION_FILE, std::ios_base::app );
+	f << time_buffer << upc << std::endl;
 	f.close();
 }
 
+/// @brief Cause this process to become a daemon
+void daemonize() {
+	int return_code = daemon(0, 0);
+	if( return_code != 0 ) {
+		log << "Failed to daemonize" << std::endl;
+		exit(3);
+	}
+}
+
+/// @brief The main function of the program
 int main(int argc, char* argv[])
 {
+	bool run_daemon = true;
+	// parse the commandline args
+	for( int i = 0; i < argc; ++i ) {
+		if( argv[i] == "--no-daemon" )
+			run_daemon = false;
+	}
+
+	// Start the daemon, if necessary
+	if( run_daemon )
+		daemonize();
+
+	// Open the log file
+	log.open( LOG_FILE, std::ios_base::app );
+
 	// Setup check
 	if( argv[1] == NULL ) {
-		printf( "Please specify the path to the device" );
+		log <<  "Please specify the path to the device" << std::endl;
 		exit(0);
 	}
 
+	// Get the device
 	char* device = NULL;
 	if( argc > 1 )
 		device = argv[1];
@@ -65,7 +107,7 @@ int main(int argc, char* argv[])
 	struct stat file_info;
 	int return_code = ::stat( device, &file_info );
 	if( return_code != 0 ) {
-		printf( "%s is not a valid device.\n", device );
+		log << device <<  " is not a valid device." << std::endl;
 		exit(1);
 	}
 
@@ -73,16 +115,16 @@ int main(int argc, char* argv[])
 	int file_descriptor = 0;
 	if( ( file_descriptor = open(device, O_RDONLY) ) == -1 )
 	{
-		printf( "%s cannot be opened.\n", device );
+		log << device << " cannot be opened." << std::endl;
 		if( getuid() != 0 )
-			printf( " Try root permissions.\n" );
+			log << " Try root permissions." << std::endl;
 		exit(2);
 	}
 
 	// Print Device Name
 	char name[256] = "Unknown";
 	ioctl( file_descriptor, EVIOCGNAME (sizeof (name)), name );
-	printf( "Reading from %s (%s)\n", device, name );
+	log << "Reading from " << device << " (" << name << ")" << std::endl;
 	
 	// Read events
 	struct input_event events[64];	
@@ -92,7 +134,7 @@ int main(int argc, char* argv[])
 		int events_read = bytes_read / sizeof(input_event);
 
 		if( events_read <= 0 ) {
-			printf( "Failed to read any events\n" );
+			log << "Failed to read any events" << std::endl;
 			exit( 0 );
 		}
 		for( int i = 0; i < events_read; ++i ) {
